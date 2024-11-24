ce5281bf422133389bcb68073c87fce850edb895
Sanjay Choudhary <iamsanjaych@gmail.com>
2024-11-24 22:55:53 +0530

    update name

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import Header from './components/LandingPage'
 import  AboutUs  from './components/AboutUs'
 import ArticleSection from './components/ArticleSection'
-import Footer from './components/footer'
+import Footer from './components/Footer'
 function App() {
   return (
     <div className='text-blueish font-open mx-auto'>
